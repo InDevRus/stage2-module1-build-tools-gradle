870056cfb916bf8c8daef76f12ec2f28adfcf0a4
indev <InDevRus@hotmail.com>
2023-08-06 19:47:11 +0500

    Reimplemented `utils`

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,8 +2,15 @@ package com.epam.utils;
 
 import org.apache.commons.lang3.math.NumberUtils;
 
+import java.math.BigDecimal;
+import java.util.Optional;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String string) {
-         return NumberUtils.toInt(string, -1) > 0;
+        return Optional.of(string)
+                .filter(NumberUtils::isCreatable)
+                .map(NumberUtils::createBigDecimal)
+                .map(decimal -> decimal.compareTo(BigDecimal.ZERO) > 0)
+                .orElse(false);
     }
 }
